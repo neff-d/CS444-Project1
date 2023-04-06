1e2ab2d04c4103656224781aaa26e1cccefb448f
Daniel Neff <neffd@oregonstate.edu>
2023-04-05 19:29:16 -0700

    April 5th

--- hellothread.c
+++ hellothread.c
@@ -3,14 +3,32 @@
 #include <pthread.h>
 
 
+void *run_threads(void *thread_id){
+    
+    for(int i = 0; i < 5; i++){
+        printf("%s: %d\n", (char *) thread_id, i);
+    }
+
+    return NULL;
+}
+
+
+
 
 int main(void){
 
+    pthread_t thread1;
+    pthread_t thread2;
 
+    printf("Launching threads\n");
 
+    pthread_create(&thread1, NULL, run_threads, "thread 1");
+    pthread_create(&thread2, NULL, run_threads, "thread 2");
 
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
+    printf("Threads complete!\n");
 
-
-
+    return 0;
 }
